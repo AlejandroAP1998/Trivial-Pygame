aa46c3632d2efab55631b4ed4475f4f9bdde9297
Alejandro Arjona <alejarperez1998@gmail.com>
2022-12-30 21:27:47 +0100

    Fondo blanco y FPS fijos

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ from pygame.locals import *
 
 WIDTH,HEIGHT = 640,640
 WINDOW = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Trivial")
+
+WHITE = (255,255,255)
+
+
+
+
+def draw():
+    WINDOW.fill(WHITE)
+    pygame.display.update()
 
 def main():
     pygame.init()
@@ -11,6 +21,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        draw()
     
     pygame.quit()
 
